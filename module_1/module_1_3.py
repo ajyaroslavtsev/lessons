fc3468b93bed9147a81f39f5c73c17500b730242
ajyaroslavtsev <ajyaroslavtsev@yandex.ru>
2024-09-09 12:10:43 +0300

    Add files via upload

---
+++ module_1/module_1_3.py
@@ -0,0 +1,10 @@
+# student: Andrey Jaroslavtsev
+
+name = 'Andrey'
+print('Name:',name)
+age = 42
+print('Age:',age)
+age = age + 5
+print('New Age:',age)
+is_student = True
+print('Is Student:',is_student)
