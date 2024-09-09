f4074b53afa3997d1e9ec4ec762c1e4b696eff7e
ajyaroslavtsev <ajyaroslavtsev@yandex.ru>
2024-09-09 14:20:26 +0300

    Add files via upload

---
+++ module_1/module_1_4.py
@@ -0,0 +1,11 @@
+# student: Andrey Jaroslavtsev
+
+
+my_string = input('Введите любой текст:')
+print('Количество символов в тексте:', len(my_string))
+print('Текст в верхнем регистре:', my_string.upper())
+print('Текст в нижнем регистре:', my_string.lower())
+print('Текст с удалёнными пробелами:', my_string.replace (' ', ''))
+print('Первый символ текста:', my_string[0])
+print('Последний символ текста:', my_string[-1])
+
