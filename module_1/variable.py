bd64b3258545553bc6abbb4d975a8c431cc0b00d
ajyaroslavtsev <ajyaroslavtsev@yandex.ru>
2024-09-09 11:50:58 +0300

    Create variable.py

---
+++ module_1/variable.py
@@ -0,0 +1,8 @@
+# student: Andrej Jaroslavtsev
+
+count_homework_is_completed =12
+count_of_hours = 1.5
+course_name = 'Python'
+task_time = count_of_hours/count_homework_is_completed
+
+print('Курс:',course_name,', всего задач:',count_homework_is_completed,', затрачено часов:',count_of_hours,', среднее время выполнения ',task_time,' часа.', sep='')
