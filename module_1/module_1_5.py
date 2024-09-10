973f85e6c180168ed5d17cc9229dd9c04754a40b
ajyaroslavtsev <ajyaroslavtsev@yandex.ru>
2024-09-10 15:31:58 +0300

    Add files via upload

---
+++ module_1/module_1_5.py
@@ -0,0 +1,10 @@
+# student: Andrey Jaroslavtsev
+
+
+immutable_var = 1, 2.0, True, 'string', [1, 2]
+print('Immutable tuple:',immutable_var)
+
+mutable_list = [1, 2.0, True, 'string']
+print('Mutable list:',mutable_list)
+mutable_list[1] = 3.0
+print('Mutable list:',mutable_list)
