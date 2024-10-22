d0fa64ec45849abca278e75ac4fd42559ad803d0
ajyaroslavtsev <ajyaroslavtsev@yandex.ru>
2024-10-22 12:50:37 +0300

    add file module_6_2.py
    Задача "Изменять нельзя получать":

---
+++ module_6/module_6_2.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+class Vehicle:
+    # - список допустимых цветов для окрашивания
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, __model, __color, __engine_power):
+        self.owner = owner                   # - владелец транспорта
+        self.__model = __model               # - модель (марка) транспорта
+        self.__engine_power = __engine_power # - мощность двигателя
+        self.__color = __color               # - название цвета
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        return f'Цвет: {self.__color}'
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f'Владелец: {self.owner}')
+
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5  # вместимость
+
+
+if __name__ == '__main__':
+    # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+    # Изначальные свойства
+    vehicle1.print_info()
+
+    # Меняем свойства (в т.ч. вызывая методы)
+    vehicle1.set_color('Pink')
+    vehicle1.set_color('BLACK')
+    vehicle1.owner = 'Vasyok'
+
+    # Проверяем что поменялось
+    vehicle1.print_info()
+
+    # Вывод на консоль:
+    '''
+    Модель: Toyota Mark II
+    Мощность двигателя: 500
+    Цвет: blue
+    Владелец: Fedos
+    Нельзя сменить цвет на Pink
+    Модель: Toyota Mark II
+    Мощность двигателя: 500
+    Цвет: BLACK
+    Владелец: Vasyok    
+    '''
