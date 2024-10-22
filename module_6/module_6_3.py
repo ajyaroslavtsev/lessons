2650899876875551bd274385282a405b0f0b9a1b
ajyaroslavtsev <ajyaroslavtsev@yandex.ru>
2024-10-22 14:44:03 +0300

    add file module_6_3.py
    Домашнее задание по теме "Множественное наследование"
    Задача "Мифическое наследование":

---
+++ module_6/module_6_3.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+class Horse:
+    def __init__(self):
+        self.x_distance = 0 # - пройденный путь
+        self.sound = 'Frrr' # - звук, который издаёт лошадь.
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+    def __init__(self):
+        self.y_distance = 0 # - высота полёта
+        self.sound = 'I train, eat, sleep, and repeat' # - звук, который издаёт орёл
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        super().__init__()
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(self.sound)
+
+
+if __name__ == '__main__':
+    p1 = Pegasus()
+
+    print(p1.get_pos())
+    p1.move(10, 15)
+    print(p1.get_pos())
+    p1.move(-5, 20)
+    print(p1.get_pos())
+
+    p1.voice()
+
+    # Вывод на консоль:
+    '''
+    (0, 0)
+    (10, 15)
+    (5, 35)
+    I train, eat, sleep, and repeat
+    '''
